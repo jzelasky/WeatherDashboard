00c7b2cf5fedce6784b6257911b9f19e0bf67221
Jessica Zelasky <jzelasky@gmail.com>
2023-01-14 09:15:16 -0500

    added reset btn and minor updates to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//README
+//publish to github pages
 
 $('#currentDay').text(' ' + dayjs().format('M/D/YYYY'));
 $('#day1').text(dayjs().add(1,'day').format('M/D/YYYY'));
@@ -14,120 +16,36 @@ var savedEl = $('#savedContainer');
     
 function formSubmitHandler(event) {
     event.preventDefault();
-    populateInfo();
+    if (searchCityEl.value){
+        searchBar();
+    }
 }
 
-function populateInfo (){
+function searchBar (){
     var city = searchCityEl.value.trim();
-    var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + APIkey
+    var city = city.split(" ");
+    for (var i = 0; i < city.length; i++){
+        city[i] = city[i].charAt(0).toUpperCase() + city[i].slice(1);
+    }
+    var city = city.join(" ");
     $('#cityName').text(city);
     if (city !== ''){
         savedCities.unshift(city);
     }
     localStorage.setItem('savedCity', JSON.stringify(savedCities));
     searchHistory();
-    fetch(geoURL)
-        .then(function (response){
-            return response.json();
-        })
-        .then(function (data){
-            var lat = data[0].lat;
-            var lon = data[0].lon;
-            var weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + APIkey + '&units=imperial'
-            fetch(weatherURL)
-                .then(function (response){
-                    return response.json();
-                })
-                .then(function (data){
-                    $('#day0temp').text(data.list[0].main.temp + ' °F')
-                    $('#day0wind').text(data.list[0].wind.speed + ' mph')
-                    $('#day0humidity').text(data.list[0].main.humidity + '%')
-                    $('#day1temp').text(data.list[6].main.temp + ' °F')
-                    $('#day1wind').text(data.list[6].wind.speed + ' mph')
-                    $('#day1humidity').text(data.list[6].main.humidity + '%')
-                    $('#day2temp').text(data.list[14].main.temp + ' °F')
-                    $('#day2wind').text(data.list[14].wind.speed + ' mph')
-                    $('#day2humidity').text(data.list[14].main.humidity + '%')
-                    $('#day3temp').text(data.list[22].main.temp + ' °F')
-                    $('#day3wind').text(data.list[22].wind.speed + ' mph')
-                    $('#day3humidity').text(data.list[22].main.humidity + '%')
-                    $('#day4temp').text(data.list[30].main.temp + ' °F')
-                    $('#day4wind').text(data.list[30].wind.speed + ' mph')
-                    $('#day4humidity').text(data.list[30].main.humidity + '%')
-                    $('#day5temp').text(data.list[38].main.temp + ' °F')
-                    $('#day5wind').text(data.list[38].wind.speed + ' mph')
-                    $('#day5humidity').text(data.list[38].main.humidity + '%')
-                    
-                    var iconURL = 'http://openweathermap.org/img/wn/'
-                    $('#icon0').attr('src', iconURL + data.list[0].weather[0].icon + '@2x.png');
-                    $('#icon1').attr('src', iconURL + data.list[6].weather[0].icon + '@2x.png');
-                    $('#icon2').attr('src', iconURL + data.list[14].weather[0].icon + '@2x.png');
-                    $('#icon3').attr('src', iconURL + data.list[22].weather[0].icon + '@2x.png');
-                    $('#icon4').attr('src', iconURL + data.list[30].weather[0].icon + '@2x.png');
-                    $('#icon5').attr('src', iconURL + data.list[38].weather[0].icon + '@2x.png');
-                })
-        })
+    populateInfo(city);
+    searchCityEl.value = ''
 }
 
-function searchHistory () {
-    savedEl.text('')
-    for (let i = 0; i < savedCities.length; i++){
-        var newBtn = document.createElement('button')
-        newBtn.textContent = savedCities[i]
-        savedEl.append(newBtn);
-        newBtn.classList.add('btn', 'btn-secondary', 'col-8', 'm-2');
-        newBtn.addEventListener('click', searchHistoryBtns);
-    }
-}
 
 function searchHistoryBtns(event) {
     city = event.target.innerText
-    var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + APIkey
     $('#cityName').text(city);
     if (city !== ''){
         savedCities.unshift(city);
     }
-    fetch(geoURL)
-        .then(function (response){
-            return response.json();
-        })
-        .then(function (data){
-            var lat = data[0].lat;
-            var lon = data[0].lon;
-            var weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + APIkey + '&units=imperial'
-            fetch(weatherURL)
-                .then(function (response){
-                    return response.json();
-                })
-                .then(function (data){
-                    $('#day0temp').text(data.list[0].main.temp + ' °F')
-                    $('#day0wind').text(data.list[0].wind.speed + ' mph')
-                    $('#day0humidity').text(data.list[0].main.humidity + '%')
-                    $('#day1temp').text(data.list[6].main.temp + ' °F')
-                    $('#day1wind').text(data.list[6].wind.speed + ' mph')
-                    $('#day1humidity').text(data.list[6].main.humidity + '%')
-                    $('#day2temp').text(data.list[14].main.temp + ' °F')
-                    $('#day2wind').text(data.list[14].wind.speed + ' mph')
-                    $('#day2humidity').text(data.list[14].main.humidity + '%')
-                    $('#day3temp').text(data.list[22].main.temp + ' °F')
-                    $('#day3wind').text(data.list[22].wind.speed + ' mph')
-                    $('#day3humidity').text(data.list[22].main.humidity + '%')
-                    $('#day4temp').text(data.list[30].main.temp + ' °F')
-                    $('#day4wind').text(data.list[30].wind.speed + ' mph')
-                    $('#day4humidity').text(data.list[30].main.humidity + '%')
-                    $('#day5temp').text(data.list[38].main.temp + ' °F')
-                    $('#day5wind').text(data.list[38].wind.speed + ' mph')
-                    $('#day5humidity').text(data.list[38].main.humidity + '%')
-                    
-                    var iconURL = 'http://openweathermap.org/img/wn/'
-                    $('#icon0').attr('src', iconURL + data.list[0].weather[0].icon + '@2x.png');
-                    $('#icon1').attr('src', iconURL + data.list[6].weather[0].icon + '@2x.png');
-                    $('#icon2').attr('src', iconURL + data.list[14].weather[0].icon + '@2x.png');
-                    $('#icon3').attr('src', iconURL + data.list[22].weather[0].icon + '@2x.png');
-                    $('#icon4').attr('src', iconURL + data.list[30].weather[0].icon + '@2x.png');
-                    $('#icon5').attr('src', iconURL + data.list[38].weather[0].icon + '@2x.png');
-                })
-        })
+    populateInfo(city);
 }
 
 function init () {
@@ -136,55 +54,90 @@ function init () {
     } else {
         var city = 'London'
     }
-    var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + APIkey
     $('#cityName').text(city);
-    if (city !== ''){
-        savedCities.unshift(city);
-    }
-    fetch(geoURL)
-        .then(function (response){
-            return response.json();
-        })
-        .then(function (data){
-            var lat = data[0].lat;
-            var lon = data[0].lon;
-            var weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + APIkey + '&units=imperial'
-            fetch(weatherURL)
-                .then(function (response){
-                    return response.json();
-                })
-                .then(function (data){
-                    $('#day0temp').text(data.list[0].main.temp + ' °F')
-                    $('#day0wind').text(data.list[0].wind.speed + ' mph')
-                    $('#day0humidity').text(data.list[0].main.humidity + '%')
-                    $('#day1temp').text(data.list[6].main.temp + ' °F')
-                    $('#day1wind').text(data.list[6].wind.speed + ' mph')
-                    $('#day1humidity').text(data.list[6].main.humidity + '%')
-                    $('#day2temp').text(data.list[14].main.temp + ' °F')
-                    $('#day2wind').text(data.list[14].wind.speed + ' mph')
-                    $('#day2humidity').text(data.list[14].main.humidity + '%')
-                    $('#day3temp').text(data.list[22].main.temp + ' °F')
-                    $('#day3wind').text(data.list[22].wind.speed + ' mph')
-                    $('#day3humidity').text(data.list[22].main.humidity + '%')
-                    $('#day4temp').text(data.list[30].main.temp + ' °F')
-                    $('#day4wind').text(data.list[30].wind.speed + ' mph')
-                    $('#day4humidity').text(data.list[30].main.humidity + '%')
-                    $('#day5temp').text(data.list[38].main.temp + ' °F')
-                    $('#day5wind').text(data.list[38].wind.speed + ' mph')
-                    $('#day5humidity').text(data.list[38].main.humidity + '%')
-                    
-                    var iconURL = 'http://openweathermap.org/img/wn/'
-                    $('#icon0').attr('src', iconURL + data.list[0].weather[0].icon + '@2x.png');
-                    $('#icon1').attr('src', iconURL + data.list[6].weather[0].icon + '@2x.png');
-                    $('#icon2').attr('src', iconURL + data.list[14].weather[0].icon + '@2x.png');
-                    $('#icon3').attr('src', iconURL + data.list[22].weather[0].icon + '@2x.png');
-                    $('#icon4').attr('src', iconURL + data.list[30].weather[0].icon + '@2x.png');
-                    $('#icon5').attr('src', iconURL + data.list[38].weather[0].icon + '@2x.png');
-                })
-        })
-    
+    populateInfo(city);
 }
 
+function populateInfo (city) {
+    var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + APIkey
+    fetch(geoURL)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        var weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + APIkey + '&units=imperial'
+        fetch(weatherURL)
+            .then(function (response){
+                return response.json();
+            })
+            .then(function (data){
+                $('#day0temp').text(data.list[0].main.temp + ' °F')
+                $('#day0wind').text(data.list[0].wind.speed + ' mph')
+                $('#day0humidity').text(data.list[0].main.humidity + '%')
+                $('#day1temp').text(data.list[8].main.temp + ' °F')
+                $('#day1wind').text(data.list[8].wind.speed + ' mph')
+                $('#day1humidity').text(data.list[8].main.humidity + '%')
+                $('#day2temp').text(data.list[16].main.temp + ' °F')
+                $('#day2wind').text(data.list[16].wind.speed + ' mph')
+                $('#day2humidity').text(data.list[16].main.humidity + '%')
+                $('#day3temp').text(data.list[24].main.temp + ' °F')
+                $('#day3wind').text(data.list[24].wind.speed + ' mph')
+                $('#day3humidity').text(data.list[24].main.humidity + '%')
+                $('#day4temp').text(data.list[32].main.temp + ' °F')
+                $('#day4wind').text(data.list[32].wind.speed + ' mph')
+                $('#day4humidity').text(data.list[32].main.humidity + '%')
+                $('#day5temp').text(data.list[39].main.temp + ' °F')
+                $('#day5wind').text(data.list[39].wind.speed + ' mph')
+                $('#day5humidity').text(data.list[39].main.humidity + '%')
+                
+                var iconURL = 'http://openweathermap.org/img/wn/'
+                $('#icon0').attr('src', iconURL + data.list[0].weather[0].icon + '@2x.png');
+                $('#icon1').attr('src', iconURL + data.list[8].weather[0].icon + '@2x.png');
+                $('#icon2').attr('src', iconURL + data.list[14].weather[0].icon + '@2x.png');
+                $('#icon3').attr('src', iconURL + data.list[22].weather[0].icon + '@2x.png');
+                $('#icon4').attr('src', iconURL + data.list[30].weather[0].icon + '@2x.png');
+                $('#icon5').attr('src', iconURL + data.list[38].weather[0].icon + '@2x.png');
+            })
+    })
+}
+
+function searchHistory () {
+    savedEl.text('')
+    let uniqueSavedCities = [];
+    savedCities.forEach((element) => {
+        if (!uniqueSavedCities.includes(element)) {
+            uniqueSavedCities.push(element);
+        }
+    });
+    for (let i = 0; i < uniqueSavedCities.length; i++){
+        var newBtn = document.createElement('button')
+        newBtn.classList.add('col-8', 'btn', 'btn-secondary', 'm-2');
+        newBtn.textContent = uniqueSavedCities[i]
+        savedEl.append(newBtn);
+        newBtn.addEventListener('click', searchHistoryBtns);
+    }
+    var resetBtn = document.createElement('button');
+    if (localStorage.getItem('savedCity')){
+        resetBtn.classList.add('col-8', 'btn', 'm-2', 'btn-outline-danger', 'btn-sm' );
+        resetBtn.textContent = 'Clear Search History';
+        savedEl.append(resetBtn);
+        resetBtn.addEventListener('click', function() {
+        localStorage.clear();
+        savedEl.text('');
+        uniqueSavedCities = [];
+        savedCities = [];
+    })
+    }
+}
+    
 searchCityBtn.addEventListener('click', formSubmitHandler);
+searchCityEl.addEventListener('keypress', function(event){
+    if (event.key === 'Enter'){
+        event.preventDefault();
+        searchCityBtn.click();
+    }
+});
 searchHistory();
 init();
